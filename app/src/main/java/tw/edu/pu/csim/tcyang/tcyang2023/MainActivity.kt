b8b864347eaa02156499f81faf51aa7eccccb01a
tcyang1971 <tcyang@pu.edu.tw>
2023-05-31 14:07:53 +0800

    v.4觸控跳出短訊息

--- app/src/main/java/tw/edu/pu/csim/tcyang/tcyang2023/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/tcyang2023/MainActivity.kt
@@ -1,6 +1,7 @@
 package tw.edu.pu.csim.tcyang.tcyang2023
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
@@ -13,11 +14,16 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.tcyang2023.ui.theme.TCYANG2023Theme
@@ -45,8 +51,13 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MapScreen() {
+    var X = remember { mutableStateOf(0f) }
+    var Y = remember { mutableStateOf(0f) }
+    val context = LocalContext.current
+
     Column(modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -59,10 +70,20 @@ fun MapScreen() {
 
     Box(
         modifier = Modifier.fillMaxSize()
+            .pointerInteropFilter { event ->
+                X.value = event.getX(0)
+                Y.value = event.getY(0)
+                true
+            }
+
     ){
         Canvas(modifier = Modifier){
             drawRect(Color.Blue, Offset(1610f, 910f), Size(40f, 40f))
             drawRect(Color.Blue, Offset(780f, 200f), Size(40f, 40f))
+        }
+
+        if (X.value>= 1610 && X.value<=1650 && Y.value>=910 && Y.value<=950){
+            Toast.makeText(context, "清水南社社區", Toast.LENGTH_SHORT).show()
         }
     }
 }
