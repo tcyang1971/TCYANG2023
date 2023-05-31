50e3ebc1c024449d5efde7eddad399559a7ba4c6
tcyang1971 <tcyang@pu.edu.tw>
2023-05-31 13:49:50 +0800

    v.2 Compose元件

--- app/src/main/java/tw/edu/pu/csim/tcyang/tcyang2023/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/tcyang2023/MainActivity.kt
@@ -3,12 +3,17 @@ package tw.edu.pu.csim.tcyang.tcyang2023
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.tcyang2023.ui.theme.TCYANG2023Theme
 
@@ -22,7 +27,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    MapScreen()
                 }
             }
         }
@@ -33,6 +39,20 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
+
+@Composable
+fun MapScreen() {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "作者:資管三A 楊子青")
+        Image(
+            painter = painterResource(id = R.drawable.map),
+            contentDescription = "清水地圖")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
